ecca0b50450d7f88bdcd2754673877ee21425023
maclariz <ian.maclaren@glasgow.ac.uk>
2026-06-26 13:55:52 +0100

    0.1.27
    addition of a stats module, additions of a pointsarray module

--- Kelvin_STEM/__init__.py
+++ Kelvin_STEM/__init__.py
@@ -1,6 +1,7 @@
 import Kelvin_STEM.io
 import Kelvin_STEM.process
 import Kelvin_STEM.viz
+import Kelvin_STEM.maths
 
 from importlib.metadata import version, PackageNotFoundError
 
---
+++ Kelvin_STEM/maths/stats.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+"""
+This module provides functions for various statistical purposes (especially in 
+quantifying grain size/shape statistics from Digital Dark Field images made from
+Machine Learning components).
+"""
+
+
+def mean_std(dataarray, weights=None):
+    """
+    Calculates weighted averages, with a sample correction for finite sample size
+    from a much larger population
+
+    Parameters
+    ----------
+    dataarray: np.ndarray
+        array of values to be analysed
+    weights: None or np.ndarray
+        None gives unweighted values equal to np.mean() and np.std()
+        array weights of same dimensions as dataarray gives weighted average
+        and standard deviation
+    Returns
+    -------
+    mean: float
+    std: float
+    """
+    if weights != None:
+        assert (
+            weights.shape == dataarray.shape
+        ), "weights must be the same shape as the data"
+        mean = np.average(dataarray, weights=weights)
+        std = np.sqrt(
+            (weights * (dataarray - mean) ** 2).sum()
+            / (weights.sum() - (weights**2).sum() / weights.sum())
+        )
+    else:
+        mean = np.mean(dataarray)
+        std = dataarray.std()
+    return mean, std
